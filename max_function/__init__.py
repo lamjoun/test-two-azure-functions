efe2c40983585b1cad76f5043a5d23de2749c8ad
Rachid LAMJOUN <93473696+lamjoun@users.noreply.github.com>
2024-11-10 23:30:57 +0100

    Update __init__.py

--- max_function/__init__.py
+++ max_function/__init__.py
@@ -20,9 +20,10 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         # Récupérer le nom du fichier et la colonne dans les paramètres de requête
         blob_name = str(req.params.get('file'))
         column = str(req.params.get('column'))
+        logging.info("---------file--------- " + blob_name)
         logging.info("---------column--------- " + column)
-        #blob_name="test1.csv"
-        #column="c1"
+        blob_name="test1.csv"
+        column="c1"
         
         #if not blob_name or not column:
         #    return func.HttpResponse("Veuillez fournir le nom du fichier et la colonne.", status_code=400)
